8f421374321769f151b5c706bb1a2c42227a1399
bitTwiddleNinja <bruceamoser@gmail.com>
2026-04-09 20:47:52 -0400

    #179: Add systemd service and install script (#191)
    Co-authored-by: bitTwiddleNinja <bmoser@pssolutions.net>

--- src/openbad/setup.py
+++ src/openbad/setup.py
@@ -32,7 +32,7 @@ CONFIG_FILES = [
     "threshold_policies.yaml",
 ]
 
-SYSTEMD_UNITS = ["openbad-broker.service"]
+SYSTEMD_UNITS = ["openbad-broker.service", "openbad.service"]
 
 DEFAULT_CONFIG_DIR = Path.home() / ".config" / "openbad"
 
